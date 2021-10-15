9fbce069c943868d8b321a71c0a959ded1d9a880
aldrinbrillante <69699828+aldrinbrillante@users.noreply.github.com>
2021-10-15 00:34:59 -0700

    identify psedocode

---
+++ identify.py
@@ -0,0 +1,14 @@
+###################################################################################################
+#                                 Identify Faces Goal:
+# identify faces and draw a box around them with their names. If known face, labeled 'unknown'
+###################################################################################################
+
+
+###################################################################################################
+#      Faces Used for Program (Will Change Names on Finalized Commit for privacy purposes):
+# Aldrin Brillante
+# 
+###################################################################################################
+
+import face_recognition
+from PIL import Image, ImageDraw #needed to draw on top of an image
